35d8f0e28088a9e7396476284d7cc1b88465704e
J Silva <72325372+silvasaurus@users.noreply.github.com>
2020-12-08 14:20:44 -0500

    added PI test/code

--- src/test/java/com/example/demo/PagesControllerTest.java
+++ src/test/java/com/example/demo/PagesControllerTest.java
@@ -2,24 +2,27 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WebMvcTest(PagesController.class)
 
-class DemoApplicationTests {
-	@Autowired
-	private MockMvc mvc;
+public class PagesControllerTest {
 
-	@Test
-	public void testHomepage() throws Exception {
-		RequestBuilder request = MockMvcRequestBuilders.get("/hello");
-		this.mvc.perform(request)
-				//.andExpect(status().isOk())
-				.andExpect(content().string("Hello World"));
-	}
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void testingPages() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/hello");
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"));
+    }
 }
